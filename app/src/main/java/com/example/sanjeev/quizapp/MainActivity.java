a8d107d4c777df18aae07ef37ee982526af91272
Sanjeev <thedevelopersanjeev@gmail.com>
2018-06-12 02:28:06 +0530

    Final Version

--- app/src/main/java/com/example/sanjeev/quizapp/MainActivity.java
+++ app/src/main/java/com/example/sanjeev/quizapp/MainActivity.java
@@ -1,17 +1,14 @@
 package com.example.sanjeev.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     EditText answerView1, answerView2;
     RadioButton[] optionRadioButtons;
@@ -24,26 +21,28 @@ public class MainActivity extends AppCompatActivity{
             R.id.option1_question8, R.id.option2_question8, R.id.option3_question8, R.id.option4_question8,
             R.id.option1_question9, R.id.option2_question9, R.id.option3_question9, R.id.option4_question9,
             R.id.option1_question0, R.id.option2_question0, R.id.option3_question0, R.id.option4_question0};
-    int totalScore;
-    String quizCompleteMessage;
+    int totalScore;                 // Total Score of the Quiz
+    String quizCompleteMessage;     // Custom Toast message to be displayed after completing the quiz
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Binding Views with Variables
         answerView1 = findViewById(R.id.answer1);
         answerView2 = findViewById(R.id.answer2);
 
         optionRadioButtons = new RadioButton[16];
-        for(int i=0; i<16; i++)
+        for (int i = 0; i < 16; i++)
             optionRadioButtons[i] = findViewById(optionRadioButtonIDs[i]);
 
         optionCheckBoxes = new CheckBox[16];
-        for(int i=0; i<16; i++)
+        for (int i = 0; i < 16; i++)
             optionCheckBoxes[i] = findViewById(optionCheckBoxesIDs[i]);
     }
 
+    // On click of submit button
     public void CalculateScore(View view) {
         totalScore = 0;
         /*
@@ -53,7 +52,7 @@ public class MainActivity extends AppCompatActivity{
          * Answer
          * Operating System
          */
-        if(answerView1.getText().toString().toLowerCase().equals("operating system"))
+        if (answerView1.getText().toString().toLowerCase().equals("operating system"))
             totalScore += 1;
         /*
          * Question Number => 2
@@ -62,7 +61,7 @@ public class MainActivity extends AppCompatActivity{
          * Answer
          * Central Processing Unit
          */
-        if(answerView2.getText().toString().toLowerCase().equals("central processing unit"))
+        if (answerView2.getText().toString().toLowerCase().equals("central processing unit"))
             totalScore += 1;
         /*
          * Question Number => 3
@@ -71,7 +70,7 @@ public class MainActivity extends AppCompatActivity{
          * Answer
          * Capacitor (Option Number 3)
          */
-        if(optionRadioButtons[2].isChecked())
+        if (optionRadioButtons[2].isChecked())
             totalScore += 1;
         /*
          * Question Number => 4
@@ -80,7 +79,7 @@ public class MainActivity extends AppCompatActivity{
          * Answer
          * Modulator Demodulator (Option Number 1)
          */
-        if(optionRadioButtons[4].isChecked())
+        if (optionRadioButtons[4].isChecked())
             totalScore += 1;
         /*
          * Question Number => 5
@@ -89,7 +88,7 @@ public class MainActivity extends AppCompatActivity{
          * Answer
          * 1 (Option Number 2)
          */
-        if(optionRadioButtons[9].isChecked())
+        if (optionRadioButtons[9].isChecked())
             totalScore += 1;
         /*
          * Question Number => 6
@@ -98,7 +97,7 @@ public class MainActivity extends AppCompatActivity{
          * Answer
          * Client (Option Number => 3)
          */
-        if(optionRadioButtons[14].isChecked())
+        if (optionRadioButtons[14].isChecked())
             totalScore += 1;
         /*
          * Question Number => 7
@@ -107,7 +106,7 @@ public class MainActivity extends AppCompatActivity{
          * Answer
          * Option 1 and 3
          */
-        if(optionCheckBoxes[0].isChecked()
+        if (optionCheckBoxes[0].isChecked()
                 && !optionCheckBoxes[1].isChecked()
                 && optionCheckBoxes[2].isChecked()
                 && !optionCheckBoxes[3].isChecked())
@@ -119,7 +118,7 @@ public class MainActivity extends AppCompatActivity{
          * Answer
          * Option 1 and 2
          */
-        if(optionCheckBoxes[4].isChecked()
+        if (optionCheckBoxes[4].isChecked()
                 && optionCheckBoxes[5].isChecked()
                 && !optionCheckBoxes[6].isChecked()
                 && !optionCheckBoxes[7].isChecked())
@@ -131,7 +130,7 @@ public class MainActivity extends AppCompatActivity{
          * Answer
          * Option Number 2 and 4
          */
-        if(!optionCheckBoxes[8].isChecked()
+        if (!optionCheckBoxes[8].isChecked()
                 && optionCheckBoxes[9].isChecked()
                 && !optionCheckBoxes[10].isChecked()
                 && optionCheckBoxes[11].isChecked())
@@ -143,13 +142,13 @@ public class MainActivity extends AppCompatActivity{
          * Answer
          * Option Number 2 and 3
          */
-        if(!optionCheckBoxes[12].isChecked()
+        if (!optionCheckBoxes[12].isChecked()
                 && optionCheckBoxes[13].isChecked()
                 && optionCheckBoxes[14].isChecked()
                 && !optionCheckBoxes[15].isChecked())
             totalScore += 1;
 
-        if(totalScore == 10)
+        if (totalScore == 10)
             quizCompleteMessage = "Perfect ! Your Score is 10/10";
         else if (totalScore < 10 && totalScore > 5)
             quizCompleteMessage = "You are close ! Your Score is " + totalScore + "/10";
